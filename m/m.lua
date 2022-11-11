84e473184e13d91a425069cbadede86f9c98ebcc
Wistpotion <wistpotion@gmail.com>
2022-11-11 13:36:31 +0100

    properly commented

--- m/m.lua
+++ m/m.lua
@@ -7,16 +7,28 @@ local debug = sys.get_engine_info().is_debug
 
 function M.reloadable(path)
 	if debug then
+		-- if the engine is in debug mode then save the
 		print(path .. " is in debug mode")
-		M.reloadables[path] = M.reloadables[path] or {}
+
+		-- get a table for the module
+		local t = M.reloadables[path] or {}
+		
+		-- purge everything in the table to not have any residual functions
+		for k, v in pairs(t) do
+			t[k] = nil
+		end
+
+		-- keep a reference to the table
+		M.reloadables[path] = t
+		
 		return M.reloadables[path]
 	else
 		return {}
 	end
 end
 
-
-local mt = { __call = function(_, path) return M.reloadable(path) end } --make it callable like m()
+-- make it callable like m(...)
+local mt = { __call = function(_, path) return M.reloadable(path) end } 
 setmetatable(M, mt)
 
 
